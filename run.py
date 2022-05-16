8398b5804d91a9e76e5d69ff01ad4fc1116caf37
gordonbates <gordonbatesofficial@gmail.com>
2022-05-16 11:09:17 +0000

    Add control structure to keep the game running using while loop

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
-print('.................Welcome to Alien Gamepy.................\n')
+print("""
+.........................................................
+.                                                       .
+.                Welcome to Alien Gamepy                .
+.                                                       .
+.........................................................
+""")
 
-print(""" 
+print("""
            \.   \.      __,-"-.__      ./   ./
        \.   \`.  \`.-'"" _,="=._ ""`-.'/  .'/   ./
         \`.  \_`-''      _,="=._      ``-'_/  .'/
@@ -18,29 +24,34 @@ print("""
      /'               ` | | | | | '               `\.
                         ` | | | '
                           ` | ' 
-                          """)
-"""
-1, credit: https://www.asciiart.eu/
-"""
+""")
+
 
 player = {'name': 'Manuel', 'attack': 10, 'heal': 10, 'health': 100}
 alien = {'name': 'Xenomorph XX121', 'attack': 15, 'health': 100}
+game_running = True
+
+while game_running == True:
+
+    print('Please Select An Action\n')
+    print('1. Attack')
+    print('2. Heal')
+
+    player_action = input()
+
+    if player_action == '1':
+        alien['health'] = alien['health'] - player['attack']
+        player['health'] = player['health'] - alien['attack']
+        print('Alien:')
+        print(alien['health'])
+        print('-------')
+        print('Player:')
+        print(player['health'])
 
 
-print('Please Select An Action\n')
-print('1. Attack')
-print('2. Heal')
-
-player_action = input()
-
-if player_action == '1':
-    alien['health'] = alien['health'] - player['attack']
-    player['health'] = player['health'] - alien['attack']
-    print(alien['health'])
-    print(player['health'])
-
-
-elif player_action == '2':
-    print('Heal')
-else:
-    print('Invalid Action')
+    elif player_action == '2':
+        print('Heal')
+    else:
+        print('Invalid Action')
+    if player['health'] <= 0:
+        game_running = False
