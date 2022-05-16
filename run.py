010dfa37f4a02105257f38465220b62b2f438ecb
gordonbates <gordonbatesofficial@gmail.com>
2022-05-16 10:26:02 +0000

    Add control structure to player and alien action using if/elif/else statements

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-print('.................Welcome to Alien Gamepy.................')
+print('.................Welcome to Alien Gamepy.................\n')
 
 print(""" 
            \.   \.      __,-"-.__      ./   ./
@@ -27,8 +27,20 @@ player = {'name': 'Manuel', 'attack': 10, 'heal': 10, 'health': 100}
 alien = {'name': 'Xenomorph XX121', 'attack': 15, 'health': 100}
 
 
-print('Please select an action')
+print('Please Select An Action\n')
 print('1. Attack')
 print('2. Heal')
 
 player_action = input()
+
+if player_action == '1':
+    alien['health'] = alien['health'] - player['attack']
+    player['health'] = player['health'] - alien['attack']
+    print(alien['health'])
+    print(player['health'])
+
+
+elif player_action == '2':
+    print('Heal')
+else:
+    print('Invalid Action')
