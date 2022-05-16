63dc2cc5b7960c097be0a3361d305e223780e40e
gordonbates <gordonbatesofficial@gmail.com>
2022-05-16 08:51:23 +0000

    Add UI and input method for player action

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
-# player_name = 'Gordon'
-# player_attack = 10
-# player_heal = 10
-# health = 100
-
-# alien = 'Xenomorph XX121'
-# player_attack = 15
-# health = 100
-
 player = {'name': 'Manuel', 'attack': 10, 'heal': 10, 'health': 100}
 alien = {'name': 'Xenomorph XX121', 'attack': 15, 'health': 100}
 
-print()
+print('Please select an action')
+print('1. Attack')
+print('2. Heal')
